34833f3c8a165e95ecc0c950c7242012fa658c9d
AzatIbragimov <azatibragimow@inbox.ru>
2021-05-21 12:22:38 +0300

    Add 2 method checkEquality & CheckNumber

--- lesson1/src/main/java/Lesson2/Lesson2.java
+++ lesson1/src/main/java/Lesson2/Lesson2.java
@@ -2,10 +2,22 @@ package Lesson2;
 
 public class Lesson2 {
     public static void main(String[] args) {
-
+        CheckEquality(11, 10);
+        CheckNumber(3);
 
         }
     public static void CheckEquality (int a, int b) {
+        boolean sum = ((a + b)>=10 && (a +b)<=20);
+        System.out.println(sum);
+    }
+
+    public static void CheckNumber (int a) {
+        if (a <0) {
+            System.out.println("Число " +a+ " -отрицательное");
+        } else {
+            System.out.println ("Число " +a+ " -положительное");
+        }
 
     }
+
 }
