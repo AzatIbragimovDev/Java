c05dc0ac03cbe24ab753cc4592e6a8b78c3368e4
AzatIbragimov <azatibragimow@inbox.ru>
2021-05-22 12:29:43 +0300

    redid the 5* task in homework

--- lesson1/src/main/java/Lesson2/Lesson2.java
+++ lesson1/src/main/java/Lesson2/Lesson2.java
@@ -6,40 +6,46 @@ public class Lesson2 {
         CheckNumber(3);
         CheckNumber2(-5);
         printWords("Hi man", 5);
-        CheckYear(300);
+        CheckYear(400);
 
     }
-    public static void CheckEquality (int a, int b) {
-        boolean sum = ((a + b)>=10 && (a +b)<=20);
+
+    public static void CheckEquality(int a, int b) {
+        boolean sum = ((a + b) >= 10 && (a + b) <= 20);
         System.out.println(sum);
     }
 
-    public static void CheckNumber (int a) {
-        if (a <0)
-            System.out.println("Число " +a+ " -отрицательное");
+    public static void CheckNumber(int a) {
+        if (a < 0)
+            System.out.println("Число " + a + " -отрицательное");
         else
-            System.out.println ("Число " +a+ " -положительное");
+            System.out.println("Число " + a + " -положительное");
     }
 
-    public static void CheckNumber2 (int a) {
+    public static void CheckNumber2(int a) {
         boolean check = a < 0;
         System.out.println(check);
     }
 
-    public static void printWords (String str, int a) {
-        for (int i =0; i < a; i++) {
-        System.out.println(str);}
-    }
-
-    public static void CheckYear (int year) {
-        boolean checkEquality1 = ((year % 4 == 0) && (year % 400 == 0));
-        boolean checkEquality2 = ((year % 100) == 0);
-        if (checkEquality1) {
-            System.out.println("true");
-        } else if (checkEquality2) {
-            System.out.println("false");
-
+    public static void printWords(String str, int a) {
+        for (int i = 0; i < a; i++) {
+            System.out.println(str);
         }
     }
 
+    public static void CheckYear(int year) {
+        boolean checkEquality1 = (year % 4 == 0);
+        boolean checkEquality2 = ((year % 100) == 0);
+        boolean checkEquality3 = (year % 400 == 0);
+        if (checkEquality1 && !checkEquality2 || checkEquality3)
+            System.out.println("true");
+
+        else if (checkEquality2)
+            System.out.println("false");
+        else System.out.println("false");
+
+
+    }
 }
+
+
